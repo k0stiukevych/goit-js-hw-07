0ccf37599c1de83bbb69b800e5932252710ff856
kostiukevechdima <kostiukevechdima@gmail.com>
2024-04-19 04:09:01 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,10 @@
-const qtt = document.querySelectorAll(".item");
+const categories = document.querySelectorAll("li.item");
 
-console.log('Number of categories:' , qtt.length);
+console.log("Number of categories:", categories.length);
 
+categories.forEach((category) => {
+  const catName = category.querySelector("h2").textContent;
+  const elementsOfCat = category.querySelectorAll("ul li");
+  console.log("Category: ", catName);
+  console.log("Elements: ", elementsOfCat.length);
+});
