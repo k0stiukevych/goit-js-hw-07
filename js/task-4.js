949d37338e2595e98a03628beb013a3bdfb54d42
kostiukevechdima <kostiukevechdima@gmail.com>
2024-04-20 23:32:30 +0300

    commit

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener('submit', toSubmit);
+
+function toSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+        return console.log("All form fields must be filled in!");
+    }
+
+    console.log(`Login: ${email.trim()}, Password: ${password.trim()}`);
+    
+    form.reset();
+}
