464519a0398c1fb68becd5c61093b6871cef678e
kostiukevechdima <kostiukevechdima@gmail.com>
2024-05-01 03:51:42 +0300

    commit

--- js/task-2.js
+++ js/task-2.js
@@ -22,20 +22,22 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
 
 const galleryList = document.querySelector(".gallery");
+const galleryItems = [];
 
 for (const image of images) {
   const itemToList = document.createElement("li");
-  galleryList.append(itemToList);
-
   const img = document.createElement("img");
-  itemToList.append(img);
 
   img.classList.add("styleforimg");
-  
   img.src = image.url;
   img.alt = image.alt;
+
+  itemToList.appendChild(img);
+  galleryItems.push(itemToList);
 }
+
+galleryList.append(...galleryItems);
--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,20 @@
 const loginForm = document.querySelector(".login-form");
 
-loginForm.addEventListener('submit', toSubmit);
+loginForm.addEventListener("submit", toSubmit);
 
 function toSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
+  event.preventDefault();
+  const form = event.target;
 
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
-    if (email === "" || password === "") {
-        return console.log("All form fields must be filled in!");
-    }
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in!");
+    return;
+  }
 
-    console.log(`Login: ${email.trim()}, Password: ${password.trim()}`);
-    
-    form.reset();
+  console.log(`Login: ${email}, Password: ${password}`);
+
+  form.reset();
 }
